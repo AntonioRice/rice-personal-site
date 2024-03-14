7b6912a79472d6e32a58c9ae90231408a4fe1d4a
Antonio Rice <ricex407@gmail.com>
2024-03-14 15:35:04 -0500

    text animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import homePhoto from "../assets/images/home.jpg";
 import AnimatedPage from "../components/AnimatedPage";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -13,33 +13,70 @@ const Home = () => {
     navigate("/photography");
   };
 
+  const text = "software engineer";
+  const defaultAnimations = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
-    <AnimatedPage>
-      <section className="relative flex h-[calc(100vh-2.5rem)] flex-col justify-center bg-[#0F1217] p-10">
-        <h1 className="mb-5 text-4xl font-bold text-[#cccccc] md:text-8xl">
-          I'm a <br />
-          <span className="text-[#ffffff]">software engineer</span>
-          <span className="text-red-500">.</span>
-        </h1>
-        <h2 className="mb-5 pb-5 text-2xl font-extralight text-[#cccccc]">
-          ...and also a wannabe photographer, which would you like to explore:
-        </h2>
-        <div className="flex animate-pulse flex-row gap-2 md:justify-start">
-          <button
-            onClick={handleLeftClick}
-            className="rounded-md bg-white px-4 py-2 transition duration-300 ease-in-out hover:bg-red-500"
-          >
-            software engineer
-          </button>
-          <button
-            onClick={handleRightClick}
-            className="rounded-md bg-white px-4 py-2 transition duration-300 ease-in-out hover:bg-red-500"
-          >
-            photographer
-          </button>
-        </div>
-      </section>
-    </AnimatedPage>
+    <AnimatePresence>
+      <AnimatedPage>
+        <section className="relative flex h-[calc(100vh-2.5rem)] flex-col justify-center bg-[#0F1217] p-10">
+          <h1 className="mb-5 text-4xl font-bold text-[#cccccc] md:text-8xl">
+            I'm a <br />
+            <motion.span
+              className="text-[#ffffff]"
+              initial="hidden"
+              animate="visible"
+              aria-hidden
+              transition={{ staggerChildren: 0.1 }}
+            >
+              {text.split("").map((char, i) => (
+                <motion.span
+                  key={i}
+                  variants={defaultAnimations}
+                  className="text-[#ffffff]"
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </motion.span>
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 1.7,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+              className="inline-block size-5 rounded-full bg-red-500 sm:size-2"
+            />
+          </h1>
+          <h2 className="mb-5 pb-5 text-2xl font-extralight text-[#cccccc]">
+            ...and also a wannabe photographer, which would you like to explore:
+          </h2>
+          <div className="flex animate-pulse flex-row gap-3 hover:gap-3 md:justify-start">
+            <motion.button
+              className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              onClick={handleLeftClick}
+            >
+              software engineer
+            </motion.button>
+            <motion.button
+              className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              onClick={handleRightClick}
+            >
+              photographer
+            </motion.button>
+          </div>
+        </section>
+      </AnimatedPage>
+    </AnimatePresence>
   );
 };
 
