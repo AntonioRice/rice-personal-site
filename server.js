0253e534da5d2c85f9976071149824e4c1f165a4
Antonio Rice <ricex407@gmail.com>
2024-03-19 16:51:16 -0500

    ...

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "dist")));
 
-// The "catchall" handler for any requests that don't match one above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const port = process.env.PORT || 80;
 app.listen(port);
-
 console.log(`Server listening on port ${port}`);
