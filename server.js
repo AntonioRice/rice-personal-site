179d4187dc688f9db1e1bc417b3fdbccf9637712
Antonio Rice <ricex407@gmail.com>
2024-03-19 16:35:42 -0500

    ...

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // The "catchall" handler for any requests that don't match one above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const port = process.env.PORT || 80;
