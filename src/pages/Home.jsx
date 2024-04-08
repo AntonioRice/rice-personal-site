46a57e8d9d039fae0fb10344e438c1f80cc9d2b8
Antonio Rice <ricex407@gmail.com>
2024-04-08 01:04:26 -0500

    main avatar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,59 +22,69 @@ const Home = () => {
   return (
     <AnimatePresence>
       <AnimatedPage>
-        <section className="relative flex h-[calc(100vh-2.5rem)] flex-col justify-center bg-[#0F1217] p-10">
-          <h1 className="mb-5 text-4xl font-bold text-[#cccccc] md:text-8xl">
-            I'm a <br />
-            <motion.span
-              className="text-[#ffffff]"
-              initial="hidden"
-              animate="visible"
-              aria-hidden
-              transition={{ staggerChildren: 0.1 }}
-            >
-              {text.split("").map((char, i) => (
-                <motion.span
-                  key={i}
-                  variants={defaultAnimations}
-                  className="text-[#ffffff]"
-                >
-                  {char}
-                </motion.span>
-              ))}
-            </motion.span>
-            <motion.div
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.8,
-                delay: 1.7,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
-              className="inline-block size-5 rounded-full bg-red-500 sm:size-2"
+        <div className="flex min-h-screen flex-col bg-[#0F1217] md:grid md:grid-cols-3">
+          <section className="order-2 flex flex-col justify-center p-10 md:order-1 md:col-span-2">
+            <h1 className="mb-5 text-4xl font-bold text-[#cccccc] md:text-8xl">
+              I'm a <br />
+              <motion.span
+                className="text-[#ffffff]"
+                initial="hidden"
+                animate="visible"
+                aria-hidden
+                transition={{ staggerChildren: 0.1 }}
+              >
+                {text.split("").map((char, i) => (
+                  <motion.span
+                    key={i}
+                    variants={defaultAnimations}
+                    className="text-[#ffffff]"
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+              </motion.span>
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 1.7,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+                className="inline-block size-5 rounded-full bg-red-500 sm:size-2"
+              />
+            </h1>
+            <h2 className="mb-5 pb-5 text-2xl font-extralight text-[#cccccc]">
+              ...and also a wannabe photographer, which would you like to
+              explore:
+            </h2>
+            <div className="flex animate-pulse flex-row gap-3 hover:gap-3 md:justify-start">
+              <motion.button
+                className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                onClick={handleLeftClick}
+              >
+                software engineer
+              </motion.button>
+              <motion.button
+                className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                onClick={handleRightClick}
+              >
+                photographer
+              </motion.button>
+            </div>
+          </section>
+          <section className="order-1 flex items-center justify-center p-10 sm:mt-[88px] md:order-2 md:col-span-1">
+            <img
+              src="src/assets/images/main-avatar.jpeg"
+              alt=""
+              className="max-h-full max-w-full sm:size-52"
             />
-          </h1>
-          <h2 className="mb-5 pb-5 text-2xl font-extralight text-[#cccccc]">
-            ...and also a wannabe photographer, which would you like to explore:
-          </h2>
-          <div className="flex animate-pulse flex-row gap-3 hover:gap-3 md:justify-start">
-            <motion.button
-              className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 400, damping: 10 }}
-              onClick={handleLeftClick}
-            >
-              software engineer
-            </motion.button>
-            <motion.button
-              className="rounded-md bg-white px-4 py-2 hover:bg-red-500"
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 400, damping: 10 }}
-              onClick={handleRightClick}
-            >
-              photographer
-            </motion.button>
-          </div>
-        </section>
+          </section>
+        </div>
       </AnimatedPage>
     </AnimatePresence>
   );
