ea4742c95a98aa9598989c253f30725eebc4da2b
AntonioRice <ricex407@gmail.com>
2024-08-05 13:32:08 -0500

    pill shadow

--- src/components/SkillPill.jsx
+++ src/components/SkillPill.jsx
@@ -1,6 +1,6 @@
 const SkillPill = ({ skill }) => {
   return (
-    <div className="inline-flex items-center rounded-xl border border-gray-500 bg-transparent px-4 py-2 text-center text-sm font-medium text-[#ffffff]">
+    <div className="inline-flex items-center rounded-xl border border-gray-500 bg-transparent px-4 py-2 text-center text-sm font-medium text-[#ffffff] shadow-xl">
       {skill}
     </div>
   );
