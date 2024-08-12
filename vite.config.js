59311ce44e8744fd4f916ea2d54db739c7d0e183
AntonioRice <ricex407@gmail.com>
2024-08-12 12:54:31 -0500

    seo optimization: vite sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.antoniorice.com",
+      routes: ["/", "software-engineer", "photographer", "cv"],
+    }),
+  ],
 });
